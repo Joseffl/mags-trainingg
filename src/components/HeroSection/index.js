e69dcbb05a2ca68cf41db8708ae0b113ca10040b
Joseph Animasaun <78298373+Joseffl@users.noreply.github.com>
2024-11-08 08:19:30 +0100

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton, SubTitleContainer, SubtitleDescription, SubtitleItem, SubtitleTitle } from './HeroStyle'
 import HeroImg from '../../images/HeroImage.png'
 import { GitHubButton} from '../Navbar/NavbarStyledComponent'
 import Typewriter from 'typewriter-effect';
-import { Bio } from '../../data/constants';
+import { Bio, subtitles } from '../../data/constants';
 
 const HeroSection = () => {
     return (
@@ -16,26 +16,61 @@ const HeroSection = () => {
                 <HeroInnerContainer >
                     <HeroLeftContainer id="Left">
                         <Title>{Bio.name}</Title>
-                        {/* <TextLoop>
-                            I am 
-                            <Span>
-                                <Typewriter
-                                    options={{
-                                        strings: Bio.roles,
-                                        autoStart: true,
-                                        loop: true,
-                                    }}
-                                /> 
-                            </Span>
-                        </TextLoop> */}
+                        
                         <SubTitle>{Bio.description}</SubTitle>
-                        <ResumeButton href="#experience" target='display'>Explore our Services</ResumeButton>
+                        
                     </HeroLeftContainer>
 
                     <HeroRightContainer id="Right">
 
                         <Img src={HeroImg} alt="hero-image" />
                     </HeroRightContainer>
+                </HeroInnerContainer>
+
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>OUR VISION</Title>
+                        
+                        <SubTitle>To be a global leader in engineering solutions, transforming industries through sustainable practices, cutting-edge technology and talent to drive sustainable growth in all sectors we serve.</SubTitle>
+                        
+                    </HeroLeftContainer>
+
+                    
+                </HeroInnerContainer>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>OUR MISSION</Title>
+                        
+                        <SubTitle>To deliver exceptional multidisciplinary engineering services high-quality engineering design 
+                            (Feasibility, FEED and detailed), procurement, and construction solutions that meet the unique needs of 
+                            our clients, fostering long-term relationships built on trust and drive sustainable development. 
+                            We are committed to excellence in project execution, prioritizing safety and environmental stewardship 
+                            while fostering long-term partnerships. Through collaboration, advanced technology, and a dedicated workforce, 
+                            we strive to exceed expectations and contribute positively to the communities we serve."</SubTitle>
+                        
+                    </HeroLeftContainer>
+
+                    
+                </HeroInnerContainer>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>OUR VALUES</Title>
+                        
+                        <SubTitle>
+                        <SubTitleContainer>
+                            {subtitles.map((subtitle, index) => (
+                                <SubtitleItem key={index}>
+                                <SubtitleTitle>{subtitle.title}</SubtitleTitle>
+                                <SubtitleDescription>{subtitle.description}</SubtitleDescription>
+                                </SubtitleItem>
+                            ))}
+                            </SubTitleContainer>
+                        </SubTitle>
+                        
+                        <ResumeButton href="#services" target='display'>Explore our Services</ResumeButton>
+                    </HeroLeftContainer>
+
+                    
                 </HeroInnerContainer>
 
             </HeroContainer>
