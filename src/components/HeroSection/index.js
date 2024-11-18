059a31654e2d44d0a9f5827335d656ebe7b4d9fb
Joseph Animasaun <78298373+Joseffl@users.noreply.github.com>
2024-11-18 16:14:21 +0100

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,81 +1,97 @@
-import React from 'react'
-import HeroBgAnimation from '../HeroBgAnimation'
-import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton, SubTitleContainer, SubtitleDescription, SubtitleItem, SubtitleTitle } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.png'
-import { GitHubButton} from '../Navbar/NavbarStyledComponent'
-import Typewriter from 'typewriter-effect';
+import React from 'react';
+import HeroBgAnimation from '../HeroBgAnimation';
+import {
+  HeroContainer,
+  HeroBg,
+  HeroLeftContainer,
+  Img,
+  HeroRightContainer,
+  HeroInnerContainer,
+  Title,
+  Span,
+  SubTitle,
+  ResumeButton,
+  SubTitleContainer,
+  SubtitleDescription,
+  SubtitleItem,
+  SubtitleTitle,
+} from './HeroStyle';
+import HeroImg from '../../images/HeroImage.png';
 import { Bio, subtitles } from '../../data/constants';
 
 const HeroSection = () => {
-    return (
-        <div id="about">
-            <HeroContainer>
-                <HeroBg>
-                    <HeroBgAnimation />
-                </HeroBg>
-                <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
-                        <Title>{Bio.name}</Title>
-                        
-                        <SubTitle>{Bio.description}</SubTitle>
-                        
-                    </HeroLeftContainer>
+  return (
+    <div id="about">
+      <HeroContainer>
+        {/* Background Animation */}
+        <HeroBg>
+          <HeroBgAnimation />
+        </HeroBg>
 
-                    <HeroRightContainer id="Right">
+        {/* Hero Content */}
+        <HeroInnerContainer>
+          <HeroLeftContainer>
+            <Title
+                style={{fontSize: '36px'}}
+            >
+                Mags Engineering Limited
+            </Title>
 
-                        <Img src={HeroImg} alt="hero-image" />
-                    </HeroRightContainer>
-                </HeroInnerContainer>
+            <SubTitle>{Bio.description}</SubTitle>
+          </HeroLeftContainer>
+          <HeroRightContainer>
+            <Img src={HeroImg} alt="hero-image" />
+          </HeroRightContainer>
+        </HeroInnerContainer>
 
-                <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
-                        <Title>OUR VISION</Title>
-                        
-                        <SubTitle>To be a global leader in engineering solutions, transforming industries through sustainable practices, cutting-edge technology and talent to drive sustainable growth in all sectors we serve.</SubTitle>
-                        
-                    </HeroLeftContainer>
+        {/* Vision Section */}
+        <HeroInnerContainer>
+          <HeroLeftContainer>
+            <Title>OUR VISION</Title>
+            <SubTitle>
+              To be a global leader in engineering solutions, transforming industries through sustainable practices,
+              cutting-edge technology, and talent to drive sustainable growth in all sectors we serve.
+            </SubTitle>
+          </HeroLeftContainer>
+        </HeroInnerContainer>
 
-                    
-                </HeroInnerContainer>
-                <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
-                        <Title>OUR MISSION</Title>
-                        
-                        <SubTitle>To deliver exceptional multidisciplinary engineering services high-quality engineering design 
-                            (Feasibility, FEED and detailed), procurement, and construction solutions that meet the unique needs of 
-                            our clients, fostering long-term relationships built on trust and drive sustainable development. 
-                            We are committed to excellence in project execution, prioritizing safety and environmental stewardship 
-                            while fostering long-term partnerships. Through collaboration, advanced technology, and a dedicated workforce, 
-                            we strive to exceed expectations and contribute positively to the communities we serve."</SubTitle>
-                        
-                    </HeroLeftContainer>
+        {/* Mission Section */}
+        <HeroInnerContainer>
+          <HeroLeftContainer>
+            <Title>OUR MISSION</Title>
+            <SubTitle>
+              To deliver exceptional multidisciplinary engineering services, high-quality engineering design
+              (Feasibility, FEED, and detailed), procurement, and construction solutions that meet the unique needs of
+              our clients. We foster long-term relationships built on trust and drive sustainable development. We are
+              committed to excellence in project execution, prioritizing safety and environmental stewardship while
+              fostering long-term partnerships. Through collaboration, advanced technology, and a dedicated workforce,
+              we strive to exceed expectations and contribute positively to the communities we serve.
+            </SubTitle>
+          </HeroLeftContainer>
+        </HeroInnerContainer>
 
-                    
-                </HeroInnerContainer>
-                <HeroInnerContainer >
-                    <HeroLeftContainer id="Left">
-                        <Title>OUR VALUES</Title>
-                        
-                        <SubTitle>
-                        <SubTitleContainer>
-                            {subtitles.map((subtitle, index) => (
-                                <SubtitleItem key={index}>
-                                <SubtitleTitle>{subtitle.title}</SubtitleTitle>
-                                <SubtitleDescription>{subtitle.description}</SubtitleDescription>
-                                </SubtitleItem>
-                            ))}
-                            </SubTitleContainer>
-                        </SubTitle>
-                        
-                        <ResumeButton href="#services" target='display'>Explore our Services</ResumeButton>
-                    </HeroLeftContainer>
+        {/* Values Section */}
+        <HeroInnerContainer>
+          <HeroLeftContainer>
+            <Title>OUR VALUES</Title>
+            <SubTitle>
+              <SubTitleContainer>
+                {subtitles.map((subtitle, index) => (
+                  <SubtitleItem key={index}>
+                    <SubtitleTitle>{subtitle.title}</SubtitleTitle>
+                    <SubtitleDescription>{subtitle.description}</SubtitleDescription>
+                  </SubtitleItem>
+                ))}
+              </SubTitleContainer>
+            </SubTitle>
+            <ResumeButton href="#services" target="display">
+              Explore our Services
+            </ResumeButton>
+          </HeroLeftContainer>
+        </HeroInnerContainer>
+      </HeroContainer>
+    </div>
+  );
+};
 
-                    
-                </HeroInnerContainer>
-
-            </HeroContainer>
-        </div>
-    )
-}
-
-export default HeroSection
+export default HeroSection;
