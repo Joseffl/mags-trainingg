1b8080dcd0c7407b26dfef0219d8ced07ab29fc2
Joseph Animasaun <78298373+Joseffl@users.noreply.github.com>
2024-11-18 16:18:15 +0100

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,61 +1,78 @@
-import React from 'react'
-import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
-import { DiCssdeck } from 'react-icons/di';
+import React from 'react';
+import { Nav, NavLink, StyledHashLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavbarStyledComponent';
 import { FaBars } from 'react-icons/fa';
-import { Bio } from '../../data/constants';
-import { Close, CloseRounded } from '@mui/icons-material';
+import LogoImg from '../../images/LogoImg.png';
 import { useTheme } from 'styled-components';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = React.useState(false);
-  const theme = useTheme()
+  const theme = useTheme();
+
   return (
     <Nav>
       <NavbarContainer>
-        <NavLogo to='/'>
-          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
-            <DiCssdeck size="3rem" /> <Span>MAGS</Span>
-          </a>
+        <NavLogo>
+          <Link to="/" style={{ display: "flex", alignItems: "center", color: "#28ac30", cursor: 'pointer' }}>
+            <img src={LogoImg} style={{ height: "4rem", width: "4rem" }} alt="Logo" /> <Span>MAGS</Span>
+          </Link>
         </NavLogo>
         <MobileIcon>
-          <FaBars onClick={() => {
-            setIsOpen(!isOpen)
-          }} />
+          <FaBars onClick={() => setIsOpen(!isOpen)} />
         </MobileIcon>
         <NavItems>
-          <NavLink href="#about">About</NavLink>
-          <NavLink href='#projects'>Projects</NavLink>
-          <NavLink href='#services'>Services</NavLink>
-          <NavLink href='#courses'>Courses</NavLink>
-          {/* <NavLink href='#education'>Education</NavLink> */}
+        <NavLink>
+            <HashLink
+              smooth
+              to="/#about"
+              style={{
+                color: "inherit",
+                fontWeight: "500",
+                cursor: "pointer",
+                textDecoration: "none",
+              }}
+            >
+              About
+            </HashLink>
+          </NavLink>
+          <NavLink as={Link} to="/projects">Projects</NavLink>
+          <NavLink>
+            <HashLink
+              smooth
+              to="/#services"
+              style={{
+                color: "inherit",
+                fontWeight: "500",
+                cursor: "pointer",
+                textDecoration: "none",
+              }}
+            >
+              Services
+            </HashLink>
+          </NavLink>
+          <NavLink as={Link} to="/training">Training</NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href="#contact" >Contact us</GitHubButton>
+          <GitHubButton href="/#contact">Contact Us</GitHubButton>
         </ButtonContainer>
-        {
-          isOpen &&
+        {isOpen && (
           <MobileMenu isOpen={isOpen}>
-            <MobileLink href="#about" onClick={() => {
-              setIsOpen(!isOpen)
-            }}>About</MobileLink>
-            <MobileLink href='#projects' onClick={() => {
-              setIsOpen(!isOpen)
-            }}>Projects</MobileLink>
-            <MobileLink href='#services' onClick={() => {
-              setIsOpen(!isOpen)
-            }}>Services</MobileLink>
-            <MobileLink href='#courses' onClick={() => {
-              setIsOpen(!isOpen)
-            }}>Courses</MobileLink>
-            {/*<MobileLink href='#education' onClick={() => {
-              setIsOpen(!isOpen)
-            }}>Education</MobileLink>*/}
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href="#contact">Contact Us</GitHubButton>
+            <MobileLink href="/#about" onClick={() => setIsOpen(false)}>About</MobileLink>
+            <MobileLink as={Link} to="/projects" onClick={() => setIsOpen(false)}>Projects</MobileLink>
+            <MobileLink href="/#services" onClick={() => setIsOpen(false)}>Services</MobileLink>
+            <MobileLink as={Link} to="/training" onClick={() => setIsOpen(false)}>Training</MobileLink>
+            <GitHubButton
+              style={{ padding: '10px 16px', background: `${theme.primary}`, color: 'white', width: 'max-content' }}
+              href="/#contact"
+            >
+              Contact Us
+            </GitHubButton>
           </MobileMenu>
-        }
+        )}
       </NavbarContainer>
     </Nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
