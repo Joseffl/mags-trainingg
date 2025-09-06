ee9964b9800a46caa949c087c14f6fed95f0b103
Joseffl <timmyjoseph505@gmail.com>
2025-09-06 20:43:41 +0100

    changes made

--- src/components/Testimonial/index.js
+++ src/components/Testimonial/index.js
@@ -147,14 +147,14 @@ const Testimonials = () => {
     },
     {
       quote:
-        "I had the privilege of undergoing HYSYS simulation training with Mr. Abiodun Adeyemi of Magg’s Engineering Services, and it was truly full of learning. The program was well-structured, highly practical, and delivered with clarity. Mr. Abiodun took time to explain complex concepts in a simple and engaging way, making the sessions both insightful and impactful. I strongly recommend Mags Engineering Limited's Trainings to anyone seeking quality, professional, and impactful training in process simulation.",
+        "I had the privilege of undergoing HYSYS simulation training with Mr. Abiodun Adeyemi of Mags Engineering Services, and it was truly full of learning. The program was well-structured, highly practical, and delivered with clarity. Mr. Abiodun took time to explain complex concepts in a simple and engaging way, making the sessions both insightful and impactful. I strongly recommend Mags Engineering Limited's Trainings to anyone seeking quality, professional, and impactful training in process simulation.",
       author: "Usman Ismail (Lab Engineer, King Fahd University of Petroleum and Minerals)",
       role: "Petroleum & Gas Engineering Participant",
     },
     {
       quote: [
         "I remember in 2022, I reached out to the CEO of Mags Engineering to discuss about my career aspirations and my interest.",
-        "He guided me and we drafted a training plan. I started with AutoCAD and drafting, learnt Aspen Hysys and also PDSM. I also went ahead to get certified in these skills.",
+        "He guided me and we drafted a training plan. I started with AutoCAD and drafting, learnt Aspen Hysys and also PDMS. I also went ahead to get certified in these skills.",
         "Today, I am currently a process engineer with a leading global LNG company.",
         "My training with Mags Engineering was both instrumental in my application, my interview and also in delivering my day to day tasks. The investment was well worth it."
       ],
